978bc600af0ae816c86d44cce4601195af50dee3
June Rhodes <jrhodes@redpointgames.com.au>
2017-09-07 01:13:54 +1000

    Add assets and test scripts for Unity

---
+++ tests/UnityTest/Assets/RunTestsComponent.cs
@@ -0,0 +1,23 @@
+﻿using HiveMP.Api;
+using HiveMP.TemporarySession.Api;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunTestsComponent : MonoBehaviour
+{
+    public void Start()
+    {
+        var temporaryClient = new TemporarySessionClient("test");
+        var promise = new HiveMPUnityPromise<TempSessionWithSecrets>(() =>
+        {
+            return temporaryClient.SessionPUT();
+        }, session =>
+        {
+            Debug.Log(session.Id);
+        }, error =>
+        {
+            Debug.LogException(error);
+        });
+    }
+}
