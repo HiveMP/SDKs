054df1cb5eb85b3bc84340e5450302ac5480388c
June Rhodes <jrhodes@redpointgames.com.au>
2017-09-07 20:47:24 +1000

    Retry if the build server gets "cubemap not supported"

--- tests/Build-UnityTests.ps1
+++ tests/Build-UnityTests.ps1
@@ -4,7 +4,7 @@ $global:ErrorActionPreference = "Stop"
 
 function Wait-For-Unity-Exit($path) {
   $linesRead = 0
-  $result = $false;
+  $outcome = "nothing";
   $running = $true;
   while ($running) {
     if (!(Test-Path $path)) {
@@ -16,10 +16,16 @@ function Wait-For-Unity-Exit($path) {
     foreach ($l in $log) {
       Write-Host $l
       if ($l.Contains("Exiting batchmode successfully")) {
-        $result = $true;
+        $outcome = "success";
+        $running = $false;
+        break;
+      } elseif ($l.Contains("cubemap not supported")) {
+        # Intermittent failure? :/
+        $outcome = "retry";
         $running = $false;
         break;
       } elseif ($l.Contains("Exiting batchmode") -or $l.Contains("Aborting batchmode")) {
+        $outcome = "failure";
         $running = $false;
         break;
       }
@@ -35,42 +41,50 @@ function Wait-For-Unity-Exit($path) {
 }
 
 function Do-Unity-Build($uPlatform, $platform) {
-  
-  echo "Cleaning tests/UnityTest..."
-  try {
-    taskkill /f /im Unity.exe
-  } catch { }
-  git clean -xdff "$PSScriptRoot\..\tests\UnityTest"
-  if ($LastExitCode -ne 0) {
-    exit 1;
-  }
-  git checkout HEAD -- "$PSScriptRoot\..\tests\UnityTest"
-  if ($LastExitCode -ne 0) {
-    exit 1;
-  }
-  
-  echo "Unpacking SDK package..."
-  Add-Type -AssemblyName System.IO.Compression.FileSystem;
-  $sdkName = (Get-Item $PSScriptRoot\..\Unity-SDK*.zip).FullName;
-  echo $sdkName
-  [System.IO.Compression.ZipFile]::ExtractToDirectory($sdkName, "$PSScriptRoot\..\tests\UnityTest\Assets\HiveMP");
+  while ($true) {
+    echo "Cleaning tests/UnityTest..."
+    try {
+      taskkill /f /im Unity.exe
+    } catch { }
+    git clean -xdff "$PSScriptRoot\..\tests\UnityTest"
+    if ($LastExitCode -ne 0) {
+      exit 1;
+    }
+    git checkout HEAD -- "$PSScriptRoot\..\tests\UnityTest"
+    if ($LastExitCode -ne 0) {
+      exit 1;
+    }
+    
+    echo "Unpacking SDK package..."
+    Add-Type -AssemblyName System.IO.Compression.FileSystem;
+    $sdkName = (Get-Item $PSScriptRoot\..\Unity-SDK*.zip).FullName;
+    echo $sdkName
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($sdkName, "$PSScriptRoot\..\tests\UnityTest\Assets\HiveMP");
 
-  echo "Building project for $platform..."
-  if (Test-Path "$PSScriptRoot\..\tests\UnityTest\Unity.log") {
-    rm -Force "$PSScriptRoot\..\tests\UnityTest\Unity.log"
-  }
-  $unity = "C:\Program Files\Unity\Editor\Unity.exe"
-  if (Test-Path "C:\Program Files\Unity_5.4.1f\Editor\Unity.exe") {
-    $unity = "C:\Program Files\Unity_5.4.1f\\Editor\Unity.exe"
-  }
-  & $unity -quit -batchmode -force-d3d9 -nographics -projectPath "$PSScriptRoot\..\tests\UnityTest" $uPlatform "$PSScriptRoot\..\tests\UnityBuilds\$platform\HiveMPTest" -logFile "$PSScriptRoot\..\tests\UnityTest\Unity.log"
-  if ($LastExitCode -ne 0) {
-    Write-Error "Unity didn't start correctly!"
-    exit 1;
-  }
-  if (!(Wait-For-Unity-Exit "$PSScriptRoot\..\tests\UnityTest\Unity.log")) {
-    Write-Error "Unity didn't build successfully!"
-    exit 1;
+    echo "Building project for $platform..."
+    if (Test-Path "$PSScriptRoot\..\tests\UnityTest\Unity.log") {
+      rm -Force "$PSScriptRoot\..\tests\UnityTest\Unity.log"
+    }
+    $unity = "C:\Program Files\Unity\Editor\Unity.exe"
+    if (Test-Path "C:\Program Files\Unity_5.4.1f\Editor\Unity.exe") {
+      $unity = "C:\Program Files\Unity_5.4.1f\\Editor\Unity.exe"
+    }
+    & $unity -quit -batchmode -force-d3d9 -nographics -projectPath "$PSScriptRoot\..\tests\UnityTest" $uPlatform "$PSScriptRoot\..\tests\UnityBuilds\$platform\HiveMPTest" -logFile "$PSScriptRoot\..\tests\UnityTest\Unity.log"
+    if ($LastExitCode -ne 0) {
+      Write-Error "Unity didn't start correctly!"
+      exit 1;
+    }
+    $outcome = (Wait-For-Unity-Exit "$PSScriptRoot\..\tests\UnityTest\Unity.log");
+    if ($outcome -eq "retry") {
+      continue;
+    }
+    if ($outcome -eq "success") {
+      return;
+    } else {
+      Write-Error "Unity didn't build successfully!"
+      exit 1;
+    }
+    break;
   }
 }
 
