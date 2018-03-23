27d99be941a6864b49c59b697d7737327568f67e
June Rhodes <jrhodes@redpointgames.com.au>
2018-03-23 18:47:24 +1100

    Make CMakeLists.txt in curl submodule as unchanged so we don't always have uncommitted changes

--- client_connect/patch.curl.ps1
+++ client_connect/patch.curl.ps1
@@ -5,3 +5,6 @@ if (!$content.Contains("#add_subdirectory(docs)"))
     $content = $content.Replace("add_subdirectory(docs)", "#add_subdirectory(docs)");
     Set-Content -Path $PSScriptRoot\curl\CMakeLists.txt -Value $content
 }
+
+cd $PSScriptRoot/curl
+git update-index --assume-unchanged ./CMakeLists.txt
