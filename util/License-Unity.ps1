ad8b349e76a2df0e9cf2a66aca39f413971cc052
June Rhodes <jrhodes@redpoint.games>
2018-07-17 12:05:11 +1000

    Try to fix up licensing

--- util/License-Unity.ps1
+++ util/License-Unity.ps1
@@ -3,7 +3,7 @@ param()
 $ErrorActionPreference = 'Stop'
 
 Write-Output "Downloading Unity licensing application..."
-Invoke-WebRequest -UseBasicParsing -Uri https://github.com/RedpointGames/UnityAutomaticLicensor/releases/download/0.3/UnityAutomaticLicensor.zip -OutFile C:\UnityAutomaticLicensor.zip
+Invoke-WebRequest -UseBasicParsing -Uri https://github.com/RedpointGames/UnityAutomaticLicensor/releases/download/0.4/UnityAutomaticLicensor.zip -OutFile C:\UnityAutomaticLicensor.zip
 
 Write-Output "Extracting Unity licensing application..."
 if (Test-Path "C:\UAL") {
@@ -14,6 +14,11 @@ if (!(Test-Path "C:\UAL")) {
 }
 [System.IO.Compression.ZipFile]::ExtractToDirectory("C:\UnityAutomaticLicensor.zip", "C:\UAL")
 
+Write-Output "Cleaning up old license files..."
+try {
+  Remove-Item -Recurse -Force C:\ProgramData\Unity
+} catch { }
+
 Write-Output "Running Unity licensing application for 5.4.1f..."
 & C:\UAL\UnityAutomaticLicensor.exe --username "$env:UNITY_LICENSE_USERNAME" --password "$env:UNITY_LICENSE_PASSWORD" --unity-path "C:\Program Files\Unity_5.4.1f\Editor\Unity.exe" --unity-version "v5.x"
 if ($LastExitCode -ne 0) {
@@ -21,8 +26,29 @@ if ($LastExitCode -ne 0) {
   exit 1
 }
 
-Write-Output "Running Unity licensing application for 2017.x and above..."
+Write-Output "Running Unity licensing application for 2017.1..."
 & C:\UAL\UnityAutomaticLicensor.exe --username "$env:UNITY_LICENSE_USERNAME" --password "$env:UNITY_LICENSE_PASSWORD" --unity-path "C:\Program Files\Unity_2017.1.1f1\Editor\Unity.exe" --unity-version "lic"
+if ($LastExitCode -ne 0) {
+  Write-Error "Licensing didn't complete successfully."
+  exit 1
+}
+
+Write-Output "Running Unity licensing application for 2017.2..."
+& C:\UAL\UnityAutomaticLicensor.exe --username "$env:UNITY_LICENSE_USERNAME" --password "$env:UNITY_LICENSE_PASSWORD" --unity-path "C:\Program Files\Unity_2017.2.0f3\Editor\Unity.exe" --unity-version "lic"
+if ($LastExitCode -ne 0) {
+  Write-Error "Licensing didn't complete successfully."
+  exit 1
+}
+
+Write-Output "Running Unity licensing application for 2017.3..."
+& C:\UAL\UnityAutomaticLicensor.exe --username "$env:UNITY_LICENSE_USERNAME" --password "$env:UNITY_LICENSE_PASSWORD" --unity-path "C:\Program Files\Unity_2017.3.0f3\Editor\Unity.exe" --unity-version "lic"
+if ($LastExitCode -ne 0) {
+  Write-Error "Licensing didn't complete successfully."
+  exit 1
+}
+
+Write-Output "Running Unity licensing application for 2018.1..."
+& C:\UAL\UnityAutomaticLicensor.exe --username "$env:UNITY_LICENSE_USERNAME" --password "$env:UNITY_LICENSE_PASSWORD" --unity-path "C:\Program Files\Unity_2018.1.7f1\Editor\Unity.exe" --unity-version "lic"
 if ($LastExitCode -ne 0) {
   Write-Error "Licensing didn't complete successfully."
   exit 1
