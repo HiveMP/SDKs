aa70aa936e8553fba43f8293c2ba14494c244517
June Rhodes <jrhodes@redpoint.games>
2018-07-28 21:42:06 +1000

    Update for UE4.18 SDK

--- sdks/UnrealEngine-4.18/Source/HiveMPSDK/HiveMPSDK.Build.cs
+++ sdks/UnrealEngine-4.18/Source/HiveMPSDK/HiveMPSDK.Build.cs
@@ -9,12 +9,6 @@ namespace UnrealBuildTool.Rules
 	{
 		public HiveMPSDK(ReadOnlyTargetRules Target) : base(Target)
         {
-            string SteamVersion = "Steamv139";
-            bool bSteamSDKFound = Directory.Exists(Target.UEThirdPartySourceDirectory + "Steamworks/" + SteamVersion) == true;
-
-            Definitions.Add("STEAMSDK_FOUND=" + (bSteamSDKFound ? "1" : "0"));
-            Definitions.Add("WITH_STEAMWORKS=" + (bSteamSDKFound ? "1" : "0"));
-
             Definitions.Add("HIVEMPSDK_PACKAGE=1");
             Definitions.Add("CLIENT_CONNECT_TARGETING_UNREAL=1");
             Definitions.Add("HAVE_LIBINTL_H=0");
@@ -36,7 +30,56 @@ namespace UnrealBuildTool.Rules
                 }
                 );
 
-            AddEngineThirdPartyPrivateStaticDependencies(Target, "Steamworks");
+            string SdkBase = Path.GetFullPath(Path.Combine(ModuleDirectory, "Private/steam"));
+
+            PrivateIncludePaths.Add(Path.Combine(SdkBase, "public"));
+
+            string LibraryPath = SdkBase;
+            string LibraryName = "steam_api";
+            
+            if (Target.Platform == UnrealTargetPlatform.Win32)
+            {
+                Definitions.Add("STEAMSDK_FOUND=1");
+                Definitions.Add("WITH_STEAMWORKS=1");
+                PublicLibraryPaths.Add(Path.Combine(LibraryPath, "win32"));
+                PublicAdditionalLibraries.Add(LibraryName + ".lib");
+                PublicDelayLoadDLLs.Add(LibraryName + ".dll");
+                RuntimeDependencies.Add(new RuntimeDependency(Path.Combine(LibraryPath, "win32/" + LibraryName + ".dll")));
+            }
+            else if (Target.Platform == UnrealTargetPlatform.Win64)
+            {
+                Definitions.Add("STEAMSDK_FOUND=1");
+                Definitions.Add("WITH_STEAMWORKS=1");
+                PublicLibraryPaths.Add(Path.Combine(LibraryPath, "win64"));
+                PublicAdditionalLibraries.Add(LibraryName + "64.lib");
+                PublicDelayLoadDLLs.Add(LibraryName + "64.dll");
+                RuntimeDependencies.Add(new RuntimeDependency(Path.Combine(LibraryPath, "win64/" + LibraryName + "64.dll")));
+            }
+            else if (Target.Platform == UnrealTargetPlatform.Mac)
+            {
+                Definitions.Add("STEAMSDK_FOUND=1");
+                Definitions.Add("WITH_STEAMWORKS=1");
+                PublicDelayLoadDLLs.Add(Path.Combine(LibraryPath, "osx/lib" + LibraryName + ".dylib"));
+                PublicAdditionalShadowFiles.Add(LibraryPath);
+                AdditionalBundleResources.Add(new UEBuildBundleResource(LibraryPath, "MacOS"));
+            }
+            else if (Target.Platform == UnrealTargetPlatform.Linux)
+            {
+                Definitions.Add("STEAMSDK_FOUND=1");
+                Definitions.Add("WITH_STEAMWORKS=1");
+
+                if (Target.LinkType == TargetLinkType.Monolithic)
+                {
+                    PublicLibraryPaths.Add(Path.Combine(LibraryPath, "linux64"));
+                    PublicAdditionalLibraries.Add(LibraryName);
+                }
+                else
+                {
+                    PublicDelayLoadDLLs.Add(Path.Combine(LibraryPath, "linux64/lib" + LibraryName + ".so"));
+                }
+                PublicAdditionalLibraries.Add(Path.Combine(LibraryPath, "linux64/lib" + LibraryName + ".so"));
+                RuntimeDependencies.Add(new RuntimeDependency(Path.Combine(LibraryPath, "linux64/lib" + LibraryName + ".so")));
+            }
         }
 	}
 }
--- tests/Build-UE4Test.ps1
+++ tests/Build-UE4Test.ps1
@@ -3,14 +3,6 @@ param([string] $Version = "4.16", [string] $Target = "")
 
 $global:ErrorActionPreference = "Stop"
 
-$OriginalLocation = $(pwd)
-
-trap {
-  Write-Output $_
-  cd $OriginalLocation
-  exit 1
-}
-
 $TestPath = "$PSScriptRoot";
 $UnrealEnginePath = "C:\Program Files\Epic Games\UE_" + $Version + "\Engine";
 $RunUAT = "$UnrealEnginePath\Build\BatchFiles\RunUAT.bat";
@@ -21,26 +13,39 @@ $ProjectName = "UnrealTest" + $Version.Replace(".", "") + ".uproject";
 function Do-Unreal-Build($Platform) {
   $OutputDir = "$PSScriptRoot\..\UnrealBuilds-$Version\$Platform";
 
-  echo "Building project for $Platform...";
-  cd $TestPath;
-  & $RunUAT BuildCookRun -nocompileeditor -installed -nop4 -project="$TestPath\$ProjectName" -cook -stage -archive -archivedirectory="$OutputDir" -package -clientconfig=Development -pak -prereqs -nodebuginfo -targetplatform=$Platform -build -unattended
-  if ($LASTEXITCODE -eq 0) {
-    return;
-  } else {
-    throw "Unreal Engine failed to build!"
+  Write-Output "Building project for $Platform...";
+  Push-Location $TestPath;
+  try {
+    & $RunUAT BuildCookRun -nocompileeditor -installed -nop4 -project="$TestPath\$ProjectName" -cook -stage -archive -archivedirectory="$OutputDir" -package -clientconfig=Development -pak -prereqs -nodebuginfo -targetplatform="$Platform" -build -unattended
+    if ($LASTEXITCODE -eq 0) {
+      return;
+    } else {
+      throw "Unreal Engine failed to build!"
+    }
+  } finally {
+    Pop-Location
   }
 }
 
-cd $PSScriptRoot\..
-
-cd $TestPath
-& $UnrealBuildTool Development Win64 -project="$TestPath\$ProjectName" -TargetType=Editor -Progress -NoHotReloadFromIDE
-if ($LASTEXITCODE -ne 0) {
-  throw "Unreal Engine failed to build!"
+Write-Output "Building UE4 editor code if necessary..."
+Push-Location $TestPath
+try {
+  $UeMinorVersion = [int]($Version.Substring($Version.IndexOf(".") + 1))
+  Write-Output "Minor UE4 version number is: $UeMinorVersion"
+  if ($UeMinorVersion -lt 20) {
+    Write-Output "& $UnrealBuildTool Development Win64 -project="$TestPath\$ProjectName" -editorrecompile -Progress -NoHotReloadFromIDE"
+    & $UnrealBuildTool $ProjectNameNoExt Development Win64 -project="$TestPath\$ProjectName" -editorrecompile -Progress -NoHotReloadFromIDE
+  } else {
+    Write-Output "& $UnrealBuildTool Development Win64 -project="$TestPath\$ProjectName" -TargetType=Editor -Progress -NoHotReloadFromIDE"
+    & $UnrealBuildTool Development Win64 -project="$TestPath\$ProjectName" -TargetType=Editor -Progress -NoHotReloadFromIDE
+  }
+  if ($LASTEXITCODE -ne 0) {
+    throw "Unreal Engine failed to build!"
+  }
+} finally {
+  Pop-Location
 }
 
 if ($Target -eq "Win64") {
   Do-Unreal-Build "Win64"
 }
-
-cd $OriginalLocation
