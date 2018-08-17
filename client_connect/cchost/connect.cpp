023d1ac95c24ef1b17fdd550ef797cc82c8d529f
June Rhodes <jrhodes@redpoint.games>
2018-08-17 21:24:19 +1000

    Use std::mutex to guard HiveMP Client Connect SDK methods being called from multiple threads
    The HiveMP Client Connect uses a coroutine style of asynchronous operation, where most of the work occurs when cc_tick is being called. We  use a mutex to guard around any SDK invocations so that structures such as callback registration lists don't get corrupted, while operations such as HTTP requests can still occur in parallel internally.

--- client_connect/cchost/connect.cpp
+++ client_connect/cchost/connect.cpp
@@ -6,6 +6,7 @@ extern "C" {
 #include "connect.impl.h"
 #include <cstdint>
 #include <cstdlib>
+#include <mutex>
 
 #include "module/hotpatching/module.h"
 
@@ -15,31 +16,45 @@ extern "C" {
 #define DLLEXPORT
 #endif
 
+// We use a global SDK lock to prevent multiple SDK methods from being called
+// at the same time, since the Client Connect SDK is internally not thread-safe.
+std::mutex global_sdk_lock;
+
 extern "C"
 {
 	DLLEXPORT void cc_init(bool log_stderr, const char* log_path)
 	{
+		std::lock_guard<std::mutex> lock(global_sdk_lock);
+
 		cci_init(log_stderr, log_path);
 	}
 
 	DLLEXPORT bool cc_tick()
 	{
+		std::lock_guard<std::mutex> lock(global_sdk_lock);
+
 		return cci_tick();
 	}
 
 	DLLEXPORT void cc_free_string(char* ptr)
 	{
+		std::lock_guard<std::mutex> lock(global_sdk_lock);
+
 		// C# can't free a C-style string on return, even though it can marshal it.
 		free(ptr);
 	}
 
 	DLLEXPORT bool cc_is_api_hotpatched(const char* api, const char* operation)
 	{
+		std::lock_guard<std::mutex> lock(global_sdk_lock);
+
 		return js_is_api_hotpatched(api, operation);
 	}
 
 	DLLEXPORT long cc_call_api_hotpatch(const char* api, const char* operation, const char* endpoint, const char* apiKey, const char* parametersAsJson)
 	{
+		std::lock_guard<std::mutex> lock(global_sdk_lock);
+
 		return js_call_api_hotpatch(
 			api,
 			operation,
@@ -51,21 +66,29 @@ extern "C"
 
 	DLLEXPORT bool cc_is_api_hotpatch_call_ready(long id)
 	{
+		std::lock_guard<std::mutex> lock(global_sdk_lock);
+
 		return js_is_api_hotpatch_call_ready(id);
 	}
 
 	DLLEXPORT const char* cc_get_api_hotpatch_result(long id)
 	{
+		std::lock_guard<std::mutex> lock(global_sdk_lock);
+
 		return js_get_api_hotpatch_result(id);
 	}
 
 	DLLEXPORT int32_t cc_get_api_hotpatch_status_code(long id)
 	{
+		std::lock_guard<std::mutex> lock(global_sdk_lock);
+
 		return js_get_api_hotpatch_status_code(id);
 	}
 
 	DLLEXPORT void cc_release_api_hotpatch_result(long id)
 	{
+		std::lock_guard<std::mutex> lock(global_sdk_lock);
+
 		return js_release_api_hotpatch_result(id);
 	}
 }
