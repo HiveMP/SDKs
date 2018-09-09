3585fcd81da8230d8182b9eaa5f0b9631579d7af
June Rhodes <jrhodes@redpoint.games>
2018-09-09 16:09:53 +1000

    Use JContainer instead of JObject for since 'object' can mean either object or array in the API

--- targets/csharp/typing/object.ts
+++ targets/csharp/typing/object.ts
@@ -8,7 +8,7 @@ export class ObjectType implements ICSharpType {
   }
   
   public getCSharpType(spec: ITypeSpec): string {
-    return 'Newtonsoft.Json.Linq.JObject';
+    return 'Newtonsoft.Json.Linq.JContainer';
   }
 
   public getNonNullableCSharpType(spec: ITypeSpec): string {
